6fb516e8f935cac602305c8557eb1de5ee217225
shizgara <shizgara85@gmail.com>
2020-09-10 13:39:34 +0300

    WebUI finish

--- app.js
+++ app.js
@@ -11,23 +11,29 @@ const app = express();
 const errorController = require("./controller/errorController");
 const adminController = require("./controller/adminContreller");
 
+//Include sequalize
+const sequalize = require("./helper/database");
 
 // Routes middleware
 const mainRoutes = require("./routes/mainRoutes");
 
 app.use(mainRoutes);
 
-
-
 /*Підключення шаблонізатора */
 app.set("view engine", "ejs");
-app.set("views", "views");/*тут треба пояснення */
+app.set("views", "views"); /*тут треба пояснення */
 app.use(bodyParser.urlencoded({ extended: false })); /*тут треба пояснення */
 
 // Підключення папки static де зберігаються шрифти,css і т.п.
 app.use(express.static(path.join(__dirname, "static")));
 
 /*Middleware for 404 Page not found */
-app.use(errorController.get404);
+// app.use(errorController.get404);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+sequalize
+  .sync()
+  .then((connectionResult) => {
+    // console.log("connectionResult ==>>",connectionResult);
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
--- controller/mainController.js
+++ controller/mainController.js
@@ -1,3 +1,61 @@
 exports.getHomePage = (req, res, next) => {
   res.render("pages/home");
 };
+
+exports.getContact = (req,res,next)=>{
+  res.render("pages/contact")
+};
+
+exports.getFaqPage = (req,res,next)=>{
+  res.render("pages/faq")
+};
+
+exports.getDeliveryPage = (req,res,next)=>{
+  res.render("pages/delivery")
+};
+
+exports.getSpecialOfferPage = (req,res,next)=>{
+  res.render("pages/special_offer")
+};
+
+exports.getCompairPage = (req,res,next)=>{
+  res.render("pages/compair")
+};
+
+exports.getComponentsPage = (req,res,next)=>{
+  res.render("pages/components")
+};
+
+exports.getForgetPassPage = (req,res,next)=>{
+  res.render("pages/forgetpass")
+};
+
+exports.getLegalNoticePage = (req,res,next)=>{
+  res.render("pages/legal_notice")
+};
+
+exports.getLoginPage = (req,res,next)=>{
+  res.render("pages/login")
+};
+
+exports.getProductDetailPage = (req,res,next)=>{
+  res.render("pages/product_detail")
+};
+
+exports.getProductSummaryPage = (req,res,next)=>{
+  res.render("pages/product_summary")
+};
+
+exports.getProductsPage = (req,res,next)=>{
+  res.render("pages/products")
+};
+
+exports.getRegisterPage = (req,res,next)=>{
+  res.render("pages/register")
+};
+
+
+exports.getTACPage = (req,res,next)=>{
+  res.render("pages/tac")
+};
+
