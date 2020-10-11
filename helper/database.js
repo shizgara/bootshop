aead7d557dc3eb8d08c91e2cec5064da86311ad1
shizgara <shizgara85@gmail.com>
2020-10-11 19:30:44 +0300

    add db usename&password in jenkins

--- helper/database.js
+++ helper/database.js
@@ -1,3 +1,5 @@
-exports.Username = 'shizgara:shizgara123';
-exports.Password = 'shizgara123';
-exports.DataBase = 'Bootshop';
+// exports.Username = 'shizgara';
+// exports.Password = 'shizgara123';
+// exports.DataBase = 'Bootshop';
+exports.DBUSERNAME = process.env.DBUSERNAME;
+exports.DBPASSWORD = process.env.DBPASSWORD;
